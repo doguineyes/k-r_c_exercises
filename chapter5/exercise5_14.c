7b839fc1024a1a52e021c2bf83b07157c46aa8da
doguineyes <doguineyes@gmail.com>
2022-11-24 20:22:28 +0800

    Add exercise 5-16

--- chapter5/exercise5_14.c
+++ chapter5/exercise5_14.c
@@ -2,8 +2,11 @@
   Exercise 5-14 Modify the sort program to handle a -r flag,
   which indicates sorting in reverse (decreasing) order. Be sure that -r works with -n.
 
-  Exercise 5-15 Add the option - f to fold upperand lower case together,
+  Exercise 5-15 Add the option -f to fold upperand lower case together,
   so that case distinctions are not made during sorting; for example, a and A compare equal.
+
+  Exercise 5-16 Add the -d ("directory order") option, which makes comparisons only on let­ters, numbers, and blanks.
+  Make sure it works in conjunction with -f .
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -132,6 +135,47 @@ int strcmp_i_ans(char* s1, char* s2) {
   return tolower(*s1) - tolower(*s2);
 }
 
+//Exercise 5-16
+int dircmp(char *s, char *t) {
+  char a, b;
+  do {
+    while (!isalnum(*s) && *s != ' ' && *s != '\0') {
+      s++;
+    }
+    while (!isalnum(*t) && *t != ' ' && *t != '\0') {
+      t++;
+    }
+    a = *s;
+    s++;
+    b = *t;
+    t++;
+    if (a == b && a == '\0') {
+      return 0;
+    }
+  } while (a == b);
+  return a - b;
+}
+
+int dircmp_i(char *s, char *t) {
+  char a, b;
+  do {
+    while (!isalnum(*s) && *s != ' ' && *s != '\0') {
+      s++;
+    }
+    while (!isalnum(*t) && *t != ' ' && *t != '\0') {
+      t++;
+    }
+    a = tolower(*s);
+    s++;
+    b = tolower(*t);
+    t++;
+    if (a == b && a == '\0') {
+      return 0;
+    }
+  } while (a == b);
+  return a - b;
+}
+
 int main(int argc, char* argv[]) {
   // assert(strcmp_i("hello", "HELLO") == 0);
   // assert(strcmp_i("hello", "Hello") == 0);
@@ -142,6 +186,7 @@ int main(int argc, char* argv[]) {
   int numeric = 0;
   int r = 1;
   int ignore = 0;
+  int direction = 0;
   char storage [MAXSTORAGE];
 
   if (argc > 1) {
@@ -152,12 +197,31 @@ int main(int argc, char* argv[]) {
         r = -1;
       } else if (strcmp("-f", argv[i]) == 0) {
         ignore = 1;
+      } else if (strcmp("-d", argv[i]) == 0) {
+        direction = 1;
       }
     }
   }
   if ((nlines = readlines(lineptr, storage, MAXLINES)) >= 0) {
+    int (*f) (void*, void*) = NULL;
+    if (numeric) {
+      f = (int (*)(void*, void*))numcmp;
+    } else if (direction) {
+      if (ignore) {
+        f = (int (*)(void*, void*))dircmp_i;
+      } else {
+        f = (int (*)(void*, void*))dircmp;
+      }
+    } else {
+      if (ignore) {
+        f = (int (*)(void*, void*))strcmp_i_ans;
+      } else {
+        f = (int (*)(void*, void*))strcmp;
+      }
+    }
     qsort_my((void **) lineptr, 0, nlines-1, 
-      (int (*)(void*, void*))(numeric ? numcmp : (ignore ? strcmp_i_ans : strcmp)), r);
+      // (int (*)(void*, void*))(numeric ? numcmp : (ignore ? strcmp_i_ans : strcmp)), r);
+      f, r);
     writelines(lineptr, nlines);
     return 0;
   } else {
